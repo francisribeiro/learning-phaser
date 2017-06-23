695ff4a1c68eae7da2855ca135eb3c378a456e52
Francis Ribeiro <franedurib@gmail.com>
2017-06-23 12:09:49 -0300

    hero animation

--- js/main.js
+++ js/main.js
@@ -52,7 +52,7 @@ function preload() {
     game.load.image('grass:1x1', 'images/grass_1x1.png')
 
     // Hero
-    game.load.image('hero', 'images/hero_stopped.png');
+    game.load.spritesheet('hero', 'images/hero.png', 36, 42);
 
     // Coin
     game.load.spritesheet('coin', 'images/coin_animated.png', 22, 22);
@@ -150,6 +150,11 @@ function Hero(game, x, y) {
 
     //Collision with the World
     this.body.collideWorldBounds = true;
+
+    this.animations.add('stop', [0]);
+    this.animations.add('run', [1, 2], 8, true); // 8fps looped
+    this.animations.add('jump', [3]);
+    this.animations.add('fall', [4]);
 }
 
 // Inherit from Phaser.Sprite
@@ -158,6 +163,12 @@ Hero.prototype.constructor = Hero;
 Hero.prototype.move = function (direction) {
     const SPEED = 200;
     this.body.velocity.x = direction * SPEED;
+    if (this.body.velocity.x < 0) {
+        this.scale.x = -1;
+    }
+    else if (this.body.velocity.x > 0) {
+        this.scale.x = 1;
+    }
 }
 Hero.prototype.jump = function () {
     const JUMP_SPEED = 600;
@@ -295,3 +306,29 @@ function _createHud() {
     this.hud.position.set(10, 10);
     this.hud.add(coinScoreImg);
 }
+
+Hero.prototype._getAnimationName = function () {
+    let name = 'stop'; // default animation
+
+    // jumping
+    if (this.body.velocity.y < 0) {
+        name = 'jump';
+    }
+    // falling
+    else if (this.body.velocity.y >= 0 && !this.body.touching.down) {
+        name = 'fall';
+    }
+    else if (this.body.velocity.x !== 0 && this.body.touching.down) {
+        name = 'run';
+    }
+
+    return name;
+};
+
+Hero.prototype.update = function () {
+    // update sprite animation, if it needs changing
+    let animationName = this._getAnimationName();
+    if (this.animations.name !== animationName) {
+        this.animations.play(animationName);
+    }
+};
